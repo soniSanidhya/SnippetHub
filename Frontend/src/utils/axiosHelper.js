ac88d70bceef463bc52881c9a9ad5988806c12d7
Sanidhya Soni <sanidhyasn314@gmail.com>
2024-12-02 01:46:26 +0530

    Remove unused files and configurations from Frontend123

--- Frontend/src/utils/axiosHelper.js
+++ Frontend/src/utils/axiosHelper.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create(
     {
-        baseURL : "http://localhost:8000/api",
+        baseURL : "https://snippethub-backend.vercel.app/api",
         headers : {
             "Content-Type" : "application/json",
         }
--- Frontend123/src/utils/axiosHelper.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://snippethub-backend.vercel.app/api/',  // Base URL for the backend API
-  withCredentials: true  // Include credentials in all requests to the backend API
-});
-
-export default api;
