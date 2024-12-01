d8ef8a5cb36d42ae40f22b87d6855ef56afa6083
Sanidhya Soni <sanidhyasn314@gmail.com>
2024-12-02 01:33:23 +0530

    Update Vite configuration to use production backend URL for API proxy

--- Frontend123/vite.config.js
+++ Frontend123/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000", // Your backend
+        target: "https://snippethub-backend.vercel.app/api/", // Your backend
         changeOrigin: true,
         secure: false,
       },
