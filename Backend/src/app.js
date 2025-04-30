20851c13ca88837332654f1ab2a842b32da3a78e
Sanidhya Soni <sanidhyasn314@gmail.com>
2025-04-30 16:03:07 +0530

    Refactor app.js to organize router imports and streamline middleware usage

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,16 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import sitemapRouter from "./Routes/sitemap.routes.js";
+// Import and use routers
+import snippetRouter from "./Routes/snippet.routes.js";
+import commentRouter from "./Routes/comment.routes.js";
+import userRouter from "./Routes/user.routes.js";
+import voteRouter from "./Routes/vote.routes.js";
+import collectionRouter from "./Routes/collection.routes.js";
+import followRouter from "./Routes/follow.routes.js";
+import categoryRouter from "./Routes/category.routes.js";
+import dashboardRouter from "./Routes/dashBoard.routes.js";
+import searchRouter from "./Routes/search.routes.js";
 dotenv.config();
 
 const app = express();
@@ -33,25 +43,20 @@ app.use(express.json({ limit: "100mb" })); // Parse JSON payloads
 app.use(express.urlencoded({ extended: true, limit: "100mb" })); // Parse URL-encoded payloads
 app.use(express.static("public")); // Serve static files
 app.use(cookieParser()); // Parse cookies
+
+app.use("/", sitemapRouter);
+app.get("/", (req, res) => {
+  res.send("Sanidhya Soni");
+});
+
 app.use(cors(corsOptions)); // Apply CORS middleware
 // Handle preflight requests
 app.options('*', cors(corsOptions));
 
 // Define a basic route
-app.get("/", (req, res) => {
-  res.send("Sanidhya Soni");
-});
 
-// Import and use routers
-import snippetRouter from "./Routes/snippet.routes.js";
-import commentRouter from "./Routes/comment.routes.js";
-import userRouter from "./Routes/user.routes.js";
-import voteRouter from "./Routes/vote.routes.js";
-import collectionRouter from "./Routes/collection.routes.js";
-import followRouter from "./Routes/follow.routes.js";
-import categoryRouter from "./Routes/category.routes.js";
-import dashboardRouter from "./Routes/dashBoard.routes.js";
-import searchRouter from "./Routes/search.routes.js";
+
+
 
 
 app.use("/api/user", userRouter);
@@ -63,7 +68,7 @@ app.use("/api/follow", followRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/dashboard" , dashboardRouter); 
 app.use("/api/search", searchRouter);
-app.use("/", sitemapRouter);
+
 
 
 
